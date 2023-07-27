3e37f1d8ee4cd8446269ecd8f133968ad76124fb
Matt Vey <matthewvey@outlook.com>
2023-07-27 14:54:08 -0230

    move spaceship

--- py_asteroids/game.py
+++ py_asteroids/game.py
@@ -1,6 +1,6 @@
 import pygame
 
-from models import GameObject
+from models import Spaceship
 from utils import load_sprite
 
 class PyAsteroids:
@@ -9,12 +9,7 @@ class PyAsteroids:
         self.screen = pygame.display.set_mode((800, 600))
         self.background = load_sprite("space", False)
         self.clock = pygame.time.Clock()
-        self.spaceship = GameObject(
-            (400, 300), load_sprite("spaceship"), (0, 0)
-        )
-        self.asteroid = GameObject(
-            (400, 300), load_sprite("asteroid"), (1, 0)
-        )
+        self.spaceship = Spaceship((400, 300))
     
     def main_loop(self):
         while True:
@@ -32,14 +27,21 @@ class PyAsteroids:
                 event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
             ):
                 quit()
+        
+        is_key_pressed = pygame.key.get_pressed()
+
+        if is_key_pressed[pygame.K_d]:
+            self.spaceship.rotate(clockwise=True)
+        elif is_key_pressed[pygame.K_a]:
+            self.spaceship.rotate(clockwise=False)
+        if is_key_pressed[pygame.K_w]:
+            self.spaceship.accelerate()
 
     def _process_game_logic(self):
-        self.spaceship.move()
-        self.asteroid.move()
+        self.spaceship.move(self.screen)
 
     def _draw(self):
         self.screen.blit(self.background, (0, 0))
         self.spaceship.draw(self.screen)
-        self.asteroid.draw(self.screen)
         pygame.display.flip()
         self.clock.tick(60)
